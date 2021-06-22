1b69afdfd034ff69fb76551e554393dfde18714c
김태윤 <kim0517@sju.ac.kr>
2021-06-23 01:38:36 +0900

    2.01 Auth

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,30 @@
-import React from "react";
-const Auth=()=><span>Auth</span>;
+import React,{useState} from "react";
+const Auth=()=>{
+    const [form,setForm]=useState({
+        email:"",
+        password:""
+    });
+    const {email,password}=form;
+    const onSubmit=(e)=>{
+        e.preventDefault();
+    }
+    const onChange=e=>{
+        const {target:{name,value}}=e;
+        setForm({
+            ...form,
+            [name]:value
+        })
+    }
+    return(
+    <>
+    <form onSubmit={onSubmit}>
+        <input type="email" name="email" placeholder="Email" required onChange={onChange} value={email}/>
+        <input type="password" name="password" placeholder="Password" onChange={onChange}required value={password}/>
+        <input type="submit"/>
+    </form>
+    <button>Continue with Google</button>
+    <button>Continue with Google</button>
+    </>
+)
+}
 export default Auth;
