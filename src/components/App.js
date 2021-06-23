caa07ff37fd70836caa012e0a711e91d47a93112
김태윤 <kim0517@sju.ac.kr>
2021-06-23 17:39:02 +0900

    2.02 Email Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import AppRouter from "components/Router";
 import {authService} from "fbase";
 function App() {
   const [isLoggedIn,setIsLoggedIn]=useState(false);
+  const [init,setInit]=useState(false);
+  useEffect(()=>{
+    authService.onAuthStateChanged(user=>{
+      if(user) setIsLoggedIn(true);
+      else setIsLoggedIn(false);
+      setInit(true);
+    })
+  },[])
   console.log(authService.currentUser);
   return(<>
-    <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+    {init? <AppRouter isLoggedIn={isLoggedIn}></AppRouter> :"initializing..."}
     <footer>&copy {new Date().getUTCDate()}</footer></>
     );
 }
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,12 +1,23 @@
 import React,{useState} from "react";
+import {authService} from "fbase";
 const Auth=()=>{
     const [form,setForm]=useState({
         email:"",
         password:""
     });
+    const [newAccount,setNewAccount]=useState(true);
+    const [error,setError]=useState("");
     const {email,password}=form;
-    const onSubmit=(e)=>{
+    const onSubmit=async(e)=>{
         e.preventDefault();
+        try{
+            let data;
+            if(newAccount) data=await authService.createUserWithEmailAndPassword(email,password);
+            else data=await authService.signInWithEmailAndPassword(email,password);
+        }catch(error){
+            console.error(error.message);
+            setError(error.message);
+        }
     }
     const onChange=e=>{
         const {target:{name,value}}=e;
@@ -15,15 +26,19 @@ const Auth=()=>{
             [name]:value
         })
     }
+    const toggleAccount=()=>setNewAccount(prev=>!prev);
     return(
     <>
     <form onSubmit={onSubmit}>
         <input type="email" name="email" placeholder="Email" required onChange={onChange} value={email}/>
         <input type="password" name="password" placeholder="Password" onChange={onChange}required value={password}/>
-        <input type="submit"/>
+        <input type="submit" value={newAccount ? "Register":"LogIn"}/>
     </form>
+    <span onClick={toggleAccount}>{newAccount ?"LogIn" : "Register"}</span>
+    <span>{error}</span>
+    <br/>
     <button>Continue with Google</button>
-    <button>Continue with Google</button>
+    <button>Continue with Github</button>
     </>
 )
 }
