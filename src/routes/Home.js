fb3fae3074f742a3de08b1751c448ac5d620b68d
김태윤 <kim0517@sju.ac.kr>
2021-06-24 16:37:51 +0900

    3.01 twit  Read

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,24 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import {dbService} from "fbase";
 const Home=()=>{
     const [twit,setTwit]=useState("");
-    const onSubmit=(e)=>{
+    const [twits,setTwits]=useState([]);
+    const getTwits=async()=>{
+        const dbTwits=await dbService.collection("twit").get();
+        dbTwits.forEach(document=>{   
+        const twitObj={
+            ...document.data(),
+            id:document.id
+        }
+            setTwits(prev=>[twitObj,...prev]);
+        });
+    }
+    useEffect(()=>{
+        getTwits();
+    },[]);
+    const onSubmit=async (e)=>{
         e.preventDefault();
-        dbService.collection("twit").add({
+        await dbService.collection("twit").add({
             twit,
             createdAt: Date.now()
         });
@@ -14,12 +28,16 @@ const Home=()=>{
         const{target:{value}}=e;
         setTwit(value);
     }
+    console.log(twits);
     return(
         <>
             <form onSubmit={onSubmit}>
                 <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} value={twit}/>
                 <input type="submit" value="Post"/>
             </form>
+            <div>
+                {twits.map(twit=><div key={twit.id}><h4>{twit.twit}</h4></div>)}
+            </div>
         </>
     )
 };
