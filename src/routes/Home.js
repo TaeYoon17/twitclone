a036eb6fce46c358e4eb64bc6c8ca76a2f4a4a94
김태윤 <kim0517@sju.ac.kr>
2021-06-24 16:06:53 +0900

    3.00 twit Create

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,26 @@
-import React from "react";
-const Home=()=><span>Home</span>;
+import React,{useState} from "react";
+import {dbService} from "fbase";
+const Home=()=>{
+    const [twit,setTwit]=useState("");
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        dbService.collection("twit").add({
+            twit,
+            createdAt: Date.now()
+        });
+        setTwit("");
+    }
+    const onChange=e=>{
+        const{target:{value}}=e;
+        setTwit(value);
+    }
+    return(
+        <>
+            <form onSubmit={onSubmit}>
+                <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} value={twit}/>
+                <input type="submit" value="Post"/>
+            </form>
+        </>
+    )
+};
 export default Home;
