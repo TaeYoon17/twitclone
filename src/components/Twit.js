58536e8e1eff95734dfef1906b6fde981e7559ed
김태윤 <kim0517@sju.ac.kr>
2021-06-25 16:29:09 +0900

    3.03 twit Delete

--- src/components/Twit.js
+++ src/components/Twit.js
@@ -1,13 +1,19 @@
+import { dbService } from "fbase";
 import React from "react";
 
 const Twit=({twitObj, isOwner})=>{
+    const onDelete=()=>{
+        const conFirm=window.confirm("You want to Delete??");
+        if(conFirm)
+            dbService.doc(`/twit/${twitObj.id}`).delete();
+    }
     return(
     <div key={twitObj.id}>
         <h4>{twitObj.text}</h4>
         {isOwner &&
         <>
         <button>Edit</button>
-        <button>Delete</button>
+        <button onClick={onDelete}>Delete</button>
         </>
         }
     </div>
